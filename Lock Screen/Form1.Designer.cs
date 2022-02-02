2b3aa6dcb6f2660cda73d9313a0b5b6aa25dba86
James Caroe <83098125+James46113@users.noreply.github.com>
2022-02-02 09:45:38 +0000

    Add files via upload

---
+++ Lock Screen/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿
+namespace Lock_Screen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.passBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // passBox
+            // 
+            this.passBox.Location = new System.Drawing.Point(72, 332);
+            this.passBox.Name = "passBox";
+            this.passBox.Size = new System.Drawing.Size(138, 20);
+            this.passBox.TabIndex = 0;
+            this.passBox.UseSystemPasswordChar = true;
+            this.passBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.passBox_KeyDown);
+            this.passBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.passBox_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.passBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox passBox;
+    }
+}
+
---
+++ Lock Screen/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace Lock_Screen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            passBox.Top = 1080 - passBox.Size.Height;
+            passBox.Left = 1920 - passBox.Size.Width;
+            FormBorderStyle = FormBorderStyle.None;
+            WindowState = FormWindowState.Maximized;
+            try 
+            {
+                BackgroundImage = Image.FromFile(Path.Combine(Application.StartupPath, @"bg.png"));
+                Console.WriteLine("png");
+            }
+            catch
+            {
+                try
+                {
+                    BackgroundImage = Image.FromFile(Path.Combine(Application.StartupPath, @"bg.jpg"));
+                    Console.WriteLine("jpg");
+                }
+                catch
+                {
+                    BackColor = Color.Black;
+                    Console.WriteLine("black");
+                }
+            }
+        }
+
+        private void passBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            Console.Write(e.KeyCode);
+            if (e.KeyCode == Keys.Enter)
+            {
+                string res = decrypt();
+                Console.WriteLine(res.Remove(res.Length-2));
+                Console.WriteLine(passBox.Text);
+                Console.WriteLine(res.Remove(res.Length-2).Equals(passBox.Text));
+                if (passBox.Text.Equals(res.Remove(res.Length-2)))
+                {
+                    Application.Exit();
+                }
+                passBox.Text = "";
+            }
+        }
+
+        private string decrypt()
+        {
+            try
+            {
+                using (FileStream fileStream = new FileStream("pw.txt", FileMode.Open))
+                {
+                    using (Aes aes = Aes.Create())
+                    {
+                        byte[] iv = new byte[aes.IV.Length];
+                        int numBytesToRead = aes.IV.Length;
+                        int numBytesRead = 0;
+                        while (numBytesToRead > 0)
+                        {
+                            int n = fileStream.Read(iv, numBytesRead, numBytesToRead);
+                            if (n == 0) break;
+
+                            numBytesRead += n;
+                            numBytesToRead -= n;
+                        }
+
+                        byte[] key =
+                        {
+                0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+                0x09, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16
+            };
+
+                        using (CryptoStream cryptoStream = new CryptoStream(
+                           fileStream,
+                           aes.CreateDecryptor(key, iv),
+                           CryptoStreamMode.Read))
+                        {
+                            using (StreamReader decryptReader = new StreamReader(cryptoStream))
+                            {
+                                return decryptReader.ReadToEnd();
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"The decryption failed. {ex}");
+                return null;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+        }
+
+        private void passBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Console.Write("¦");
+
+            if (e.KeyChar == '¦')
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
