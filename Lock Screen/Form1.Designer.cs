d55082b640498e8a57d9c87a924f99adee2dd3cb
James Caroe <jamescaroe@gmail.com>
2022-02-02 12:31:54 +0000

    changes

--- Lock Screen/Form1.Designer.cs
+++ Lock Screen/Form1.Designer.cs
@@ -30,24 +30,43 @@ namespace Lock_Screen
         private void InitializeComponent()
         {
             this.passBox = new System.Windows.Forms.TextBox();
+            this.signOutButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // passBox
             // 
-            this.passBox.Location = new System.Drawing.Point(72, 332);
+            this.passBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.passBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.passBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passBox.ForeColor = System.Drawing.SystemColors.Control;
+            this.passBox.Location = new System.Drawing.Point(12, 12);
             this.passBox.Name = "passBox";
-            this.passBox.Size = new System.Drawing.Size(138, 20);
+            this.passBox.Size = new System.Drawing.Size(138, 15);
             this.passBox.TabIndex = 0;
             this.passBox.UseSystemPasswordChar = true;
             this.passBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.passBox_KeyDown);
             this.passBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.passBox_KeyPress);
+            // 
+            // signOutButton
+            // 
+            this.signOutButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.signOutButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.signOutButton.ForeColor = System.Drawing.SystemColors.Control;
+            this.signOutButton.Location = new System.Drawing.Point(48, 32);
+            this.signOutButton.Name = "signOutButton";
+            this.signOutButton.Size = new System.Drawing.Size(59, 24);
+            this.signOutButton.TabIndex = 1;
+            this.signOutButton.Text = "Sign Out";
+            this.signOutButton.UseVisualStyleBackColor = false;
+            this.signOutButton.Click += new System.EventHandler(this.signOutButton_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(166, 68);
+            this.Controls.Add(this.signOutButton);
             this.Controls.Add(this.passBox);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -62,6 +81,7 @@ namespace Lock_Screen
         #endregion
 
         private System.Windows.Forms.TextBox passBox;
+        private System.Windows.Forms.Button signOutButton;
     }
 }
 
--- Lock Screen/Form1.cs
+++ Lock Screen/Form1.cs
@@ -9,6 +9,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Security.Cryptography;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace Lock_Screen
 {
@@ -21,12 +23,15 @@ namespace Lock_Screen
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Cursor.Hide();
-            passBox.Top = 1080 - passBox.Size.Height;
-            passBox.Left = 1920 - passBox.Size.Width;
+            passBox.Top = 1080 - passBox.Height;
+            passBox.Left = 1920 - passBox.Width;
+            signOutButton.Top = 1080 - signOutButton.Height;
+            signOutButton.Left = 0;
             FormBorderStyle = FormBorderStyle.None;
-            WindowState = FormWindowState.Maximized;
-            try 
+            //WindowState = FormWindowState.Maximized;
+            this.WindowState = FormWindowState.Maximized;
+            lockKeys();
+            try
             {
                 BackgroundImage = Image.FromFile(Path.Combine(Application.StartupPath, @"bg.png"));
                 Console.WriteLine("png");
@@ -52,10 +57,10 @@ namespace Lock_Screen
             if (e.KeyCode == Keys.Enter)
             {
                 string res = decrypt();
-                Console.WriteLine(res.Remove(res.Length-2));
+                Console.WriteLine(res.Remove(res.Length - 2));
                 Console.WriteLine(passBox.Text);
-                Console.WriteLine(res.Remove(res.Length-2).Equals(passBox.Text));
-                if (passBox.Text.Equals(res.Remove(res.Length-2)))
+                Console.WriteLine(res.Remove(res.Length - 2).Equals(passBox.Text));
+                if (passBox.Text.Equals(res.Remove(res.Length - 2)))
                 {
                     Hide();
                     Application.Exit();
@@ -123,6 +128,103 @@ namespace Lock_Screen
             if (e.KeyChar == '¦')
             {
                 Application.Exit();
+            }
+        }
+        private void signOutButton_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Do you really want to sign out?", "Sign Out?", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+            {
+                Process.Start("shutdown.exe", "/l");
+            }
+        }
+
+
+        public delegate IntPtr LowLevelKeyboardProcDelegate(int nCode, IntPtr wParam, ref KBDLLHOOKSTRUCT lParam);
+
+        [DllImport("kernel32", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        [DllImport("user32", EntryPoint = "SetWindowsHookEx", SetLastError = true)]
+        public static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProcDelegate lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32", EntryPoint = "UnhookWindowsHookEx", SetLastError = true)]
+        public static extern bool UnhookWindowsHookEx(IntPtr hHook);
+
+        [DllImport("user32", EntryPoint = "CallNextHookEx", SetLastError = true)]
+        public static extern IntPtr CallNextHookEx(IntPtr hHook, int nCode, IntPtr wParam, ref KBDLLHOOKSTRUCT lParam);
+
+        public const int WH_KEYBOARD_LL = 13;
+
+        /*code needed to disable start menu*/
+        [DllImport("user32.dll")]
+        private static extern int FindWindow(string className, string windowText);
+        [DllImport("user32.dll")]
+        private static extern int ShowWindow(int hwnd, int command);
+
+        private const int SW_HIDE = 0;
+        private const int SW_SHOW = 1;
+
+        public struct KBDLLHOOKSTRUCT
+        {
+            public int vkCode;
+            public int scanCode;
+            public int flags;
+            public int time;
+            public int dwExtraInfo;
+        }
+
+        public static IntPtr intLLKey;
+
+        public IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, ref KBDLLHOOKSTRUCT lParam)
+        {
+            bool blnEat = false;
+
+            try
+            {
+                switch (wParam.ToInt64())
+                {
+                    case 256:
+                    case 257:
+                    case 260:
+                    case 261:
+
+                        //Alt+Tab, Alt+Esc, Ctrl+Esc, Windows Key,
+                        blnEat = ((lParam.vkCode == 9) && (lParam.flags == 32))  // alt+tab
+                            | ((lParam.vkCode == 27) && (lParam.flags == 32)) // alt+esc
+                            | ((lParam.vkCode == 27) && (lParam.flags == 0))  // ctrl+esc
+                            | ((lParam.vkCode == 91) && (lParam.flags == 1))  // left winkey
+                            | ((lParam.vkCode == 92) && (lParam.flags == 1))
+                            | ((lParam.vkCode == 73) && (lParam.flags == 0));
+
+                        break;
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            if (blnEat == true)
+            {
+                return (IntPtr)1;
+            }
+            else
+            {
+                return CallNextHookEx(intLLKey, nCode, wParam, ref lParam);
+            }
+        }
+
+        private void lockKeys()
+        {
+            using (ProcessModule curModule = Process.GetCurrentProcess().MainModule)
+            {
+                intLLKey = SetWindowsHookEx(WH_KEYBOARD_LL, LowLevelKeyboardProc, GetModuleHandle(curModule.ModuleName), 0);
+            }
+            if (intLLKey.ToInt64() == 0)
+            {
+                throw new Win32Exception();
             }
         }
     }
